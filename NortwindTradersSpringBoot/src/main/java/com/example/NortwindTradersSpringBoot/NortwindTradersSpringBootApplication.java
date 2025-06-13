7ce54be6a949c1927e2013198bc345331d0eb56b
Vilayat Gajar <gajarovv2002@gmail.com>
2025-06-13 13:31:34 -0400

    Connected Spring to project

---
+++ NortwindTradersSpringBoot/src/main/java/com/example/NortwindTradersSpringBoot/NortwindTradersSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.NortwindTradersSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NortwindTradersSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NortwindTradersSpringBootApplication.class, args);
+	}
+
+}
